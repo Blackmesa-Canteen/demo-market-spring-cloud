82cc2bb9b66c8ccb85deed9577a3464326ae8cd7
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2021-12-01 18:37:22 +0800

    1. 配置了部分在线配置管理,还有gateway demo

---
+++ demomarket-gateway/src/test/java/com/example/demomarket/gateway/DemomarketGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demomarket.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomarketGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
